2578ab0955471312ff369449fd12bf5272f523e1
Boshika Tara <boshikatara@gmail.com>
2016-01-22 19:57:47 -0800

    Adding comarator function for sort

--- MoreFunctional.js
+++ MoreFunctional.js
@@ -1,10 +1,10 @@
+/////////////////////////////////////////////////
+
 /** Functions as Units of Behavior
 Data hiding aka encapsulation, can be done by storing and passing discrete
 units of behavior
 Example of that is Array Indexing
 **/
-
-/////////////////////////////////////////////////
 var indexArray = function(collection, index) {
   return collection[index];
 };
@@ -13,3 +13,9 @@ var indexArray = function(collection, index) {
 indexArray([1,2,3,4], 2)
 //will return 3
 //////////////////////////////////////////////////
+
+/** Comparator
+Sort method w/o a function does a string comparison, to avoid this 
+a comparator function needs to be passed into sort
+**/
+
