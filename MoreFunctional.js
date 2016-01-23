a47e464afc369f6be6295c67217b712814c465b3
Boshika Tara <boshikatara@gmail.com>
2016-01-22 19:52:16 -0800

    Added a new file

---
+++ MoreFunctional.js
@@ -0,0 +1,15 @@
+/** Functions as Units of Behavior
+Data hiding aka encapsulation, can be done by storing and passing discrete
+units of behavior
+Example of that is Array Indexing
+**/
+
+/////////////////////////////////////////////////
+var indexArray = function(collection, index) {
+  return collection[index];
+};
+
+//Example
+indexArray([1,2,3,4], 2)
+//will return 3
+//////////////////////////////////////////////////
