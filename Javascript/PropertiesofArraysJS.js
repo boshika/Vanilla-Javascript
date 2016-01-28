f1b55eec821fb1063846aba3719d3c03e6925762
Boshika Tara <boshikatara@gmail.com>
2016-01-28 13:46:38 -0800

    Added an example for splice.

--- Javascript/PropertiesofArraysJS.js
+++ Javascript/PropertiesofArraysJS.js
@@ -91,3 +91,12 @@ console.log(findep);
 //   'Danny',
 //   'Jennifer' ]
 
+//splice()
+
+var itDiv = ["Mike","Clayton","Terrill","Raymond","Cynthia","Danny","Jennifer"];
+var dmpDept = itDiv.splice(3,3);
+var cisDept = itDiv;
+console.log(itDiv);   //[ 'Mike', 'Clayton', 'Terrill', 'Jennifer' ]
+console.log(dmpDept); // Raymond,Cynthia,Danny
+console.log(cisDept); // Mike,Clayton,Terrill,Jennifer
+
