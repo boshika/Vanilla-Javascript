f9cee02f065d4dfc63fa8bea6db8041bd3dc122f
Boshika Tara <boshikatara@gmail.com>
2016-01-28 13:42:20 -0800

    Added example for methods used to create new arrays from existing arrays

--- Javascript/PropertiesofArraysJS.js
+++ Javascript/PropertiesofArraysJS.js
@@ -31,3 +31,63 @@ nums[0] = 400;
 
 console.log(samenums[0]);
 
+//JS ACCESSOR FUNCTIONS
+//indexof and lastindexof
+
+var names = ["David", "Cynthia", "Raymond", "Clayton", "Jennifer"];
+putstr("Enter a name to search for: ");
+var name = readline();
+var position = names.indexOf(name);
+
+if (position >= 0) {
+ console.log("Found " + name + " at position " + position);
+} else {
+ console.log(name + " not found in array.");
+}
+
+//Found Cynthia at position 1
+//Joe not found in array
+
+var names = ["David", "Mike", "Cynthia", "Raymond", "Clayton", "Mike",
+ "Jennifer"];
+var name = "Mike";
+var firstPos = names.indexOf(name);
+console.log("First found " + name + " at position " + firstPos);
+var lastPos = names.lastIndexOf(name);
+console.log("Last found " + name + " at position " + lastPos);
+
+//First found Mike at position 1
+//Last found Mike at position 5
+
+//String Representations of Arrays: join(), and toString()
+
+var names = ["David", "Cynthia", "Raymond", "Clayton", "Mike", "Jennifer"];
+var namestr = names.join();
+console.log(namestr); // David,Cynthia,Raymond,Clayton,Mike,Jennifer
+namestr = names.toString();
+console.log(namestr); // David,Cynthia,Raymond,Clayton,Mike,Jennifer
+
+//Creating New Arrays from Existing Arrays: splice(), and concat()
+
+var cisDept = ["Mike", "Clayton", "Terrill", "Danny", "Jennifer"];
+var dmpDept = ["Raymond", "Cynthia", "Bryan"];
+
+var itdept = cisDept.concat(dmpDept);
+// console.log(itdept);
+//[ 'Mike', 'Clayton', 'Terrill', 'Danny', 'Jennifer', 'Raymond', 'Cynthia', 'Bryan' ]
+var findep= itdept.concat(cisDept);
+console.log(findep);
+// [ 'Mike',
+//   'Clayton',
+//   'Terrill',
+//   'Danny',
+//   'Jennifer',
+//   'Raymond',
+//   'Cynthia',
+//   'Bryan',
+//   'Mike',
+//   'Clayton',
+//   'Terrill',
+//   'Danny',
+//   'Jennifer' ]
+
