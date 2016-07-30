afd4d2fa978f671da967aacb056c3d16c59aeca5
Boshika Tara <boshikatara@gmail.com>
2016-07-29 17:35:59 -0700

    New Project

---
+++ Functions/IIFE.js
@@ -0,0 +1,7 @@
+/* An IIFE is basically where a function is defined, and
+then immediately called. It is quite useful in compressing code and reducing scope lookups for
+more effective execution of code.
+*/
+
+
+
